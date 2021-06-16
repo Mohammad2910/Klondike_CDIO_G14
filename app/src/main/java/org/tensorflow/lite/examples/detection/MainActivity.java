93a16dbecb832cf35f537dc3b5e74e548463debf
MerithzDTU <s195480@student.dtu.dk>
2021-06-16 11:42:57 +0200

    MainActivity fjol

--- app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -153,7 +153,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 //        tracker.trackResults(mappedRecognitions, new Random().nextInt());
-//        trackingOverlay.postInvalidate();
-        imageView.setImageBitmap(bitmap);
+//        trackingOverlay.postInvalidate()
     }
 }
